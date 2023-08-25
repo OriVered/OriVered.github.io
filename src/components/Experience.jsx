6c08a6dd3c72451e0e579bb4f25888fcdb8eacf4
OriVered <ori.vered@gmail.com>
2023-08-25 12:50:07 +0300

    update

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -102,7 +102,7 @@ const Experience = () => {
               ease-in-out"
               onClick={() =>
                 window.open(
-                  'resume link', //paste the link to your resume here
+                  'src/assets/personal/Ori-Vered-CV.pdf', //paste the link to your resume here
                   '_blank'
                 )
               }
